7de37324a37b0d89525c927a5c782875b71009fb
sam <sblan45@gmail.com>
2021-05-09 21:07:42 -0400

    adds accordion component with active features

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   showModal = false;
+  items = [
+    { title: "why is the sky blue?", content: "Because of air"},
+    { title: "Where is Boston?", content: "Boston is in Massachusetts"},
+    { title: "What color is the cat?", content: "the cat is orange color"}
+  ]
 
   constructor() { }
 
